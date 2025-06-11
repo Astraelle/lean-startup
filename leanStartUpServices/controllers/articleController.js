fe9f44114f5bb02f3696f02fbdfdc1cbd9894516
Astraelle <charlie-pierre@outlook.fr>
2025-06-12 00:35:29 +0200

    ajout de routes pour la récupération d'un seul article

--- leanStartUpServices/controllers/articleController.js
+++ leanStartUpServices/controllers/articleController.js
@@ -31,7 +31,7 @@ exports.createArticle = async (req, res) =>{
 
 exports.getAllArticles = async (req, res) =>{
     try {
-        const article = await Article.find().populate('authorId', 'username email');
+        const article = await Article.find().populate('authorId', 'username email').sort({createdAt: -1});
         res.status(200).json(article);
     } catch (error) {
         res.status(500).json({
@@ -39,3 +39,14 @@ exports.getAllArticles = async (req, res) =>{
         });
     }
 }
+
+exports.getArticleById = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ message: 'Article introuvable' });
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ message: 'Erreur serveur' });
+  }
+};
--- leanStartUpServices/routes/article.js
+++ leanStartUpServices/routes/article.js
@@ -2,11 +2,12 @@ const express = require('express');
 const Article = require('../models/article.model');
 const auth = require('../middlewares/auth');
 const isAdmin = require('../middlewares/admin');
-const { getAllArticles, createArticle } = require('../controllers/articleController');
+const { getAllArticles, createArticle, getArticleById } = require('../controllers/articleController');
 
 const router = express.Router();
 
 router.post('/create-article', auth, isAdmin, createArticle);
 router.get('/get-article', getAllArticles);
+router.get('/:id', getArticleById);
 
 module.exports = router;
