a2d42e62a41a149e32b258012644b75d5f77e0b4
Astraelle <charlie-pierre@outlook.fr>
2025-06-27 03:30:30 +0200

    changes

--- leanstartupweb/app/temoignages/page.tsx
+++ leanstartupweb/app/temoignages/page.tsx
@@ -1,17 +1,3 @@
-<<<<<<< Updated upstream
-export default function Temoignages(){
-    return(
-        <>
-            <section className="pt-40 h-screen w-full">
-                <div className='grid justify-items-center'>
-                    <p className='bg-[#32BF84] w-40 h-10 text-center text-white grid items-center rounded-3xl -rotate-1'>RESSOURCES</p>
-                </div>
-                <h2 className="xl:text-6xl font-bold text-center pt-10">Témoignages de nos kollabers</h2>
-                <p className="text-center pt-10">Lorem ipsum dolor sit amet consectetur. Tempor diam interdum arcu gravida ipsum nisl nec pellentesque.<br/> Ullamcorper quis nunc lacinia faucibus accumsan proin sapien.</p>
-            </section>
-        </>
-    )
-=======
 import Breadcrumb from '../../(components)/ui/Breadcrumb';
 
 export default function Temoignages(){
@@ -31,5 +17,4 @@ export default function Temoignages(){
             </div>
         </>
     )
->>>>>>> Stashed changes
 }
